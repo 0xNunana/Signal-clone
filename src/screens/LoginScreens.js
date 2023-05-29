41f37fa73cdc3d4cae0cb957c64531414130c5a9
0xNunana <kudayapaul@gmail.com>
2023-05-29 19:17:37 +0000

    login implemented

--- src/screens/LoginScreens.js
+++ src/screens/LoginScreens.js
@@ -1,15 +1,34 @@
 import { KeyboardAvoidingView, Platform, StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {Button,Input, Image} from 'react-native-elements'
 import { StatusBar } from 'expo-status-bar'
 import {useNavigation} from '@react-navigation/native'
+import {onAuthStateChanged,signInWithEmailAndPassword} from 'firebase/auth'
+import { auth } from '../../firebase'
 
 const LoginScreens = () => {
     const navigation = useNavigation()
     const [email,setEmail]=useState('')
     const [password,setPassword]=useState("")
-    const SignIn =()=>{
+    useEffect(()=>{
+        
+            const unsubscribe =onAuthStateChanged(auth, (user) => {
+                if (user) {
+                    console.log(user)
+                    navigation.replace("Home")
+                  // User is signed in, see docs for a list of available properties
+                  // https://firebase.google.com/docs/reference/js/auth.user
+                 // const uid = user.uid;
+                  // ...
+                } 
+              });
 
+              return unsubscribe;
+        
+    },[])
+  
+    const SignIn=()=>{
+signInWithEmailAndPassword(auth,email,password).catch((error)=>alert(error))
     }
   return (
     <KeyboardAvoidingView 
@@ -21,7 +40,7 @@ const LoginScreens = () => {
     
     <View style={{width:300}}>
         <Input placeholder='Email' type="email" autoFocus value={email} onChangeText={(text) =>setEmail(text)} />
-        <Input placeholder='Password' type="password" autoFocus secureTextEntry value={password} onChangeText={(text)=>setPassword(text)}/>
+        <Input placeholder='Password' type="password" autoFocus secureTextEntry value={password} onChangeText={(text)=>setPassword(text)} onSubmitEditing={SignIn}/>
     </View>
     
     <Button title="Login" containerStyle={{width:200, marginTop:10}} onPress={SignIn}/>
