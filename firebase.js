3b2e6bdab1af81cc9c7e926f6cef27c322f499c4
0xNunana <kudayapaul@gmail.com>
2023-05-29 15:07:08 +0000

    firebase config

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD2Kk3Mtx3CKdprom_HZQfyOAE_RJeDEdg",
+    authDomain: "signal-clone-c6020.firebaseapp.com",
+    projectId: "signal-clone-c6020",
+    storageBucket: "signal-clone-c6020.appspot.com",
+    messagingSenderId: "126981162060",
+    appId: "1:126981162060:web:f53a39aa1667926783a0e9"
+  };
